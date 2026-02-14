fe0a7dd1300af3f90a236a8d9cf50df824075022
Francisco Casado <hola@franciscocasado.cl>
2026-02-14 13:03:58 -0300

    feat(nvim): add markdown-preview.nvim

--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -2,7 +2,7 @@ return {
   { import = 'plugins.edit' },
   { import = 'plugins.git' },
   { import = 'plugins.langs' },
-  --  { import = 'plugins.misc' },
   { import = 'plugins.nav' },
   { import = 'plugins.ui' },
+  { import = 'plugins.utils' },
 }
