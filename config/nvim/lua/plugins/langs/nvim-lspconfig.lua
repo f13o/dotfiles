2484980070d7d727dc3c1e5c58513f6b2e80f6fa
Francisco Casado <hola@franciscocasado.cl>
2026-04-25 18:41:29 -0400

    feat(nvim): add sourcekit-lsp

--- config/nvim/lua/plugins/langs/nvim-lspconfig.lua
+++ config/nvim/lua/plugins/langs/nvim-lspconfig.lua
@@ -64,6 +64,38 @@ return {
 
     vim.lsp.enable('ruby_lsp')
 
+    -- Swift
+
+    vim.lsp.config('sourcekit', {
+      cmd = { 'sourcekit-lsp' },
+      filetypes = { 'swift' },
+      root_markers = {
+        '.git',
+        'compile_commands.json',
+        '.sourcekit-lsp',
+        'Package.swift',
+      },
+      get_language_id = function(_, ftype)
+        return ftype
+      end,
+      capabilities = {
+        workspace = {
+          didChangeWatchedFiles = {
+            dynamicRegistration = true,
+          },
+        },
+        textDocument = {
+          diagnostic = {
+            dynamicRegistration = true,
+            relatedDocumentSupport = true,
+          },
+        },
+      },
+      mason = false,
+    })
+
+    vim.lsp.enable('sourcekit')
+
     -- TOML
     vim.lsp.config('taplo', {})
   end,
